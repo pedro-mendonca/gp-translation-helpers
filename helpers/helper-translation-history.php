4d271ce7ef5f3e0f2a559e8fd134e9d35d37a97b
Tosin Oguntuyi <tcubed03@gmail.com>
2022-01-26 14:40:26 +0100

    Add link to translation in history tab

--- helpers/helper-translation-history.php
+++ helpers/helper-translation-history.php
@@ -2,8 +2,8 @@
 
 class Helper_History extends GP_Translation_Helper {
 
-	public $priority = 2;
-	public $title = 'History';
+	public $priority          = 2;
+	public $title             = 'History';
 	public $has_async_content = true;
 
 	function activate() {
@@ -22,18 +22,21 @@ class Helper_History extends GP_Translation_Helper {
 			return;
 		}
 
-		$translations  = GP::$translation->find_many_no_map(
+		$translations = GP::$translation->find_many_no_map(
 			array(
 				'translation_set_id' => $translation_set->id,
-				'original_id' => $this->data['original_id'],
+				'original_id'        => $this->data['original_id'],
 			)
 		);
 
-		usort( $translations, function ( $t1, $t2 ) {
-			$cmp_prop_t1 = $t1->date_modified ?: $t1->date_added;
-			$cmp_prop_t2 = $t2->date_modified ?: $t2->date_added;
-			return $cmp_prop_t1 < $cmp_prop_t2;
-		} );
+		usort(
+			$translations,
+			function ( $t1, $t2 ) {
+				$cmp_prop_t1 = $t1->date_modified ?: $t1->date_added;
+				$cmp_prop_t2 = $t2->date_modified ?: $t2->date_added;
+				return $cmp_prop_t1 < $cmp_prop_t2;
+			}
+		);
 
 		$this->set_count( $translations );
 
@@ -42,7 +45,7 @@ class Helper_History extends GP_Translation_Helper {
 
 	function async_output_callback( $translations ) {
 		if ( $translations ) {
-			$output = '<table id="translation-history-table">';
+			$output  = '<table id="translation-history-table">';
 			$output .= '<thead>';
 			$output .= '<tr><th>Date</th><th>Translation</th><th>Added by</th><th>Last modified by</th>';
 			$output .= '</thead>';
@@ -51,12 +54,12 @@ class Helper_History extends GP_Translation_Helper {
 				$date_and_time = is_null( $translation->date_modified ) ? $translation->date_added : $translation->date_modified;
 				$date_and_time = explode( ' ', $date_and_time );
 
-				$user = get_userdata( $translation->user_id );
+				$user               = get_userdata( $translation->user_id );
 				$user_last_modified = get_userdata( $translation->user_id_last_modified );
 
 				if ( ( '' == $translation->translation_1 ) && ( '' == $translation->translation_2 ) &&
-				     ( '' == $translation->translation_3 ) && ( '' == $translation->translation_4 ) &&
-				     ( '' == $translation->translation_5 ) ) {
+					 ( '' == $translation->translation_3 ) && ( '' == $translation->translation_4 ) &&
+					 ( '' == $translation->translation_5 ) ) {
 						$output_translation = $translation->translation_0;
 				} else {
 					$output_translation = '<ul>';
@@ -68,11 +71,12 @@ class Helper_History extends GP_Translation_Helper {
 					$output_translation .= '</ul>';
 				}
 
-				$output .= sprintf( '<tr class="preview status-%1$s"><td title="%2$s">%3$s</td><td>%4$s</td><td>%5$s</td><td>%6$s</td></tr>',
+				$output .= sprintf(
+					'<tr class="preview status-%1$s"><td title="%2$s">%3$s</td><td>%4$s</td><td>%5$s</td><td>%6$s</td></tr>',
 					esc_attr( $translation->status ),
-					esc_attr( $translation->date_modified ?: $translation->date_added ) ,
-					esc_html( $date_and_time[0] ) ,
-					$output_translation,
+					esc_attr( $translation->date_modified ?: $translation->date_added ),
+					esc_html( $date_and_time[0] ),
+					'<a href="' . $this->get_translation_permalink() . '">' . $output_translation . '</a>',
 					$user ? esc_html( $user->user_login ) : '&mdash;',
 					$user_last_modified ? esc_html( $user_last_modified->user_login ) : '&mdash;'
 				);
@@ -83,5 +87,19 @@ class Helper_History extends GP_Translation_Helper {
 
 	function empty_content() {
 		return 'No translation history for this string';
+	}
+
+	public function get_translation_permalink() {
+		$translation_permalink = gp_url_project_locale(
+			$this->data['project'],
+			$this->data['locale_slug'],
+			$this->data['translation_set_slug'],
+			array(
+				'filters[status]'         => 'either',
+				'filters[original_id]'    => $this->data['original_id'],
+				'filters[translation_id]' => $this->data['translation_id'],
+			)
+		);
+		return $translation_permalink;
 	}
 }
